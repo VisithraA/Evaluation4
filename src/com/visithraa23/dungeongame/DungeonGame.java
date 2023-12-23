a0d066e7d1459911c914b1cc0bc475f8942b4ed7
VisithraA <vithra7904@gmail.com>
2023-12-23 11:01:43 +0530

    commit

---
+++ src/com/visithraa23/dungeongame/DungeonGame.java
@@ -0,0 +1,35 @@
+package com.visithraa23.dungeongame;
+
+import java.util.Scanner;
+
+public class DungeonGame {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		DungeonGame game = new DungeonGame();
+		game.Question1();
+
+	}
+
+	private void Question1() {
+		System.out.println("Dimensions of Dungeon:");
+		int row = sc.nextInt();
+		int column = sc.nextInt();
+		System.out.println("Position of Adventurer:");
+		int adRow = sc.nextInt();
+		int adCol = sc.nextInt();
+		System.out.println("Position of Gold:");
+		int goldRow = sc.nextInt();
+		int goldCol = sc.nextInt();
+
+		if (adRow > row && adCol > column) {
+			System.out.println("Adventure position is limit exceeded");
+		} else if (goldRow > row && goldCol > column) {
+			System.out.println("Gold position is limit exceeded");
+		} else {
+			int res = Math.abs(adRow - goldRow) + Math.abs(adCol - goldCol);
+			System.out.println("Minimum Number of Steps " + res);
+		}
+
+	}
+}
