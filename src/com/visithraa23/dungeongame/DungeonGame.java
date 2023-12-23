4141ecda434bb8359315090528e755960ef241f9
VisithraA <vithra7904@gmail.com>
2023-12-23 11:49:34 +0530

    commit

--- src/com/visithraa23/dungeongame/DungeonGame.java
+++ src/com/visithraa23/dungeongame/DungeonGame.java
@@ -7,28 +7,42 @@ public class DungeonGame {
 
 	public static void main(String[] args) {
 		DungeonGame game = new DungeonGame();
-		game.Question1();
+		game.Question2();
 
 	}
 
-	private void Question1() {
+	private void Question2() {
 		System.out.println("Dimensions of Dungeon:");
 		int row = sc.nextInt();
 		int column = sc.nextInt();
 		System.out.println("Position of Adventurer:");
 		int adRow = sc.nextInt();
 		int adCol = sc.nextInt();
+		System.out.println("Position of Monster");
+		int monRow=sc.nextInt();
+		int monCol=sc.nextInt();
 		System.out.println("Position of Gold:");
 		int goldRow = sc.nextInt();
 		int goldCol = sc.nextInt();
 
-		if (adRow > row && adCol > column) {
+		if (adRow > row || adCol > column) {
 			System.out.println("Adventure position is limit exceeded");
-		} else if (goldRow > row && goldCol > column) {
+		} else if (goldRow > row || goldCol > column) {
 			System.out.println("Gold position is limit exceeded");
-		} else {
-			int res = Math.abs(adRow - goldRow) + Math.abs(adCol - goldCol);
-			System.out.println("Minimum Number of Steps " + res);
+		} 
+		else if(monRow>row||monCol>column){
+			System.out.println("monster position is limit exceeded");
+		}
+		else {
+			int AdventurePath = Math.abs(adRow - goldRow) + Math.abs(adCol - goldCol);
+			int MonsterPath=Math.abs(monRow-goldRow)+Math.abs(monCol-goldCol);
+			
+			if(MonsterPath<AdventurePath)
+				System.out.println("No Possible solution" );
+			else {
+				System.out.println("Minimum Number of Steps "+AdventurePath);
+			}
+			
 		}
 	}
 }
